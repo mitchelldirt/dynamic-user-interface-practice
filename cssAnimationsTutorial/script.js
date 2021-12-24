63bb99dea4b755a3506f5a28cc1d630b2f53c9fd
Mitchell Mudd <mitchellmudd@outlook.com>
2021-12-24 11:45:14 -0500

    Learned about animations and keyframes!

--- cssAnimationsTutorial/script.js
+++ cssAnimationsTutorial/script.js
@@ -2,3 +2,10 @@ var trigger = document.querySelector(".trigger");
 trigger.onclick = function () {
     trigger.classList.toggle("clicked");
 };
+var html = document.querySelector("html");
+html.onclick = function () {
+    var body = document.querySelector("body");
+    var box = document.createElement("div");
+    box.classList.add("box");
+    body.appendChild(box);
+};
--- cssAnimationsTutorial/script.ts
+++ cssAnimationsTutorial/script.ts
@@ -3,3 +3,11 @@ const trigger: HTMLDivElement = document.querySelector(".trigger");
 trigger.onclick = () => {
     trigger.classList.toggle("clicked");
 }
+
+const html: HTMLElement = document.querySelector("html");
+html.onclick = () => {
+    const body = document.querySelector("body")
+    const box: HTMLDivElement = document.createElement("div");
+    box.classList.add("box");
+    body.appendChild(box);
+}
