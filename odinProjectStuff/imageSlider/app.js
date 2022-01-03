304e226ff3cbd025b5e0e15f897dc662736470cf
Mitchell Mudd <mitchellmudd@outlook.com>
2022-01-03 18:13:26 +0000

    Fully functional - might make it look prettier

--- odinProjectStuff/imageSlider/app.js
+++ odinProjectStuff/imageSlider/app.js
@@ -1,56 +1,61 @@
-//TODO: create a function for removing and adding the checked radio button
 //TODO: Have the picture change every 5 seconds
+window.addEventListener("load", () => {
+    let radioButtons = document.getElementsByName("pictureSelector");
+    let image = document.getElementById("image")
+    let right = document.getElementById("right");
+    let left = document.getElementById("left");
 
-let radioButtons = document.getElementsByName("pictureSelector");
-let image = document.getElementById("image")
-let right = document.getElementById("right");
-let left = document.getElementById("left");
 
-
-function addPictureSliding() {
-    for (let thing of radioButtons) {
-        thing.addEventListener("click", () => {
-            let currentImageSrc = image.getAttribute("src");
-            imageNumber = +currentImageSrc[8]
-            let checkedRadioButton = document.getElementById(`pic${imageNumber}`);
-            checkedRadioButton.removeAttribute("checked", "");
-            let pictureNumber = thing.id;
-            image.setAttribute("src", `imgs/${pictureNumber}.jpeg`);
-            let uncheckedRadioButton = document.getElementById(pictureNumber);
-            uncheckedRadioButton.setAttribute("checked", "")
-        })
-    }
-}
-
-function arrows(direction) {
-    let imageNumber;
-    let currentImageSrc = image.getAttribute("src");
-    imageNumber = +currentImageSrc[8]
-    let checkedRadioButton = document.getElementById(`pic${imageNumber}`);
-    checkedRadioButton.removeAttribute("checked", "");
-    if (direction === "right") {
-        imageNumber += 1;
-        if (imageNumber === 7) {
-            imageNumber = 1;
-        }
-    } else {
-        imageNumber -= 1
-        if (imageNumber === 0) {
-            imageNumber = 6;
+    function addPictureSliding() {
+        for (let thing of radioButtons) {
+            thing.addEventListener("click", () => {
+                let currentImageSrc = image.getAttribute("src");
+                imageNumber = +currentImageSrc[8]
+                let checkedRadioButton = document.getElementById(`pic${imageNumber}`);
+                checkedRadioButton.removeAttribute("checked", "");
+                let pictureNumber = thing.id;
+                image.setAttribute("src", `imgs/${pictureNumber}.jpeg`);
+                let uncheckedRadioButton = document.getElementById(pictureNumber);
+                uncheckedRadioButton.checked = true;
+            })
         }
     }
-    let uncheckedRadioButton = document.getElementById(`pic${imageNumber}`);
-    uncheckedRadioButton.setAttribute("checked", "")
-    image.setAttribute("src", `imgs/pic${imageNumber}.jpeg`);
-}
+
+    function arrows(direction) {
+        let imageNumber;
+        let currentImageSrc = image.getAttribute("src");
+        imageNumber = +currentImageSrc[8]
+        let checkedRadioButton = document.getElementById(`pic${imageNumber}`);
+        checkedRadioButton.removeAttribute("checked", "");
+        if (direction === "right") {
+            imageNumber += 1;
+            if (imageNumber === 7) {
+                imageNumber = 1;
+            }
+        } else {
+            imageNumber -= 1
+            if (imageNumber === 0) {
+                imageNumber = 6;
+            }
+        }
+        let uncheckedRadioButton = document.getElementById(`pic${imageNumber}`);
+        uncheckedRadioButton.checked = true;
+        image.setAttribute("src", `imgs/pic${imageNumber}.jpeg`);
+    }
 
 
 
-addPictureSliding();
-right.addEventListener('click', () => {
-    arrows("right");
-});
+    addPictureSliding();
+    right.addEventListener('click', () => {
+        arrows("right");
+    });
 
-left.addEventListener('click', () => {
-    arrows("left");
-}); 
+    left.addEventListener('click', () => {
+        arrows("left");
+    });
+
+    const pictureInterval = setInterval(function () {
+        arrows("right")
+    }, 5000);
+})
+
